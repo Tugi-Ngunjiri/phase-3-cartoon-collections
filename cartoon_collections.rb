dcafd69dd7de1fd7a623f3c7910a44f8eb439bc3
tugi <blueqwinn@gmail.com>
2023-02-10 21:54:20 +0300

    first commit

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,40 @@
 def roll_call_dwarves# code an argument here
   # Your code here
+  ["Dumbo" ,"Grumpy", "Smarty" , "Tempty"]
+puts " Dumbo" , "Grumpy" , "Smarty" , "Tempty"
 end
 
 def summon_captain_planet# code an argument here
   # Your code here
+planteer_calls= ["earthy", "watery", "wind"y, "firery", "hearty"]
+ return planteer_calls
+summon_captain_planet(planteer_calls)
+# =>  ["earthy!", "watery!", "windy!", "firery!", "hearty!"]
+
 end
 
 def long_planeteer_calls# code an argument here
   # Your code here
+short_words =["get", "pick" , "come"]
+long_planeteer_calls(short_words)
+# => false
+
+assorted_words = ["one", "sit", "other", "peep"]
+long_planeteer_calls(assorted_words)
+# => true
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+find_the_cheese(snacks)
+# => "cheddar"
+
+soup =["seafood soup", "butternut", "tomato", "lamb pollock"]
+find_the_cheese(soup)
+# => "butternut"
+
+ingredients =["onion", "tomato" ,"butter"]
+find_the_cheese(ingredients)
+# => nil
 end
